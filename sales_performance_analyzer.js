b5604c4f6202b4c13a8f7d894f646d34bf988f18
Haferm98 <105403350+Haferm98@users.noreply.github.com>
2024-09-07 19:14:38 -0400

    Create determinePerformanceRating function

--- sales_performance_analyzer.js
+++ sales_performance_analyzer.js
@@ -19,3 +19,25 @@ function calculateAverageSales(sales)
     salesData.forEach(sales => (totalSales += salesData.sales));
     averageSales = (totalSales/sales.length);
 }
+
+//Task 2: Create a Function to Determine Performance Rating
+function determinePerformanceRating(averageSales)
+{
+    if (averageSales > 10000)
+    {
+        return "Excellent";
+    }
+    else if (averageSales >= 7000)
+    {
+        return "Good";
+    }
+    else if (averageSales >= 4000)
+    {
+        return "Satisfactory";
+    }
+    else if (averageSales < 4000)
+    {
+        return "Needs Ipmrovement";
+    }
+}
+
